7df3eec34467b888a3d912b765918eb6e7e155e1
Jeffrey Alvarez <kuritsu@gmail.com>
2021-06-07 15:05:06 -0500

    Update aws bucket.

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -18,8 +18,8 @@ resource "aws_instance" "web" {
   count         = var.instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id     = var.subnet_ids[count.index % 3]
-  vpc_security_group_ids = ["sg-04f1d6a2db29ad162"]
+  subnet_id     = var.subnet_ids[count.index % 2]
+  vpc_security_group_ids = ["sg-01bad3f16778cb6c9"]
   tags = {
     Name = "web${count.index + 1}"
   }
@@ -28,5 +28,52 @@ resource "aws_instance" "web" {
 apt-get update -y
 apt-get install nginx -y
 EOD
+  lifecycle {
+    ignore
+  }
+}
 
+# Create a new load balancer
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  access_logs {
+    bucket        = "foo"
+    bucket_prefix = "bar"
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 8000
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.foo.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
 }
